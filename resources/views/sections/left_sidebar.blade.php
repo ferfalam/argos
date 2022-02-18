3f8d99de3526e7b2ba4eb09f273ffa62ce793664
abdoullahio <89782745+abdoullahio@users.noreply.github.com>
2022-02-18 11:36:06 +0100

    Update left_sidebar.blade.php

--- resources/views/sections/left_sidebar.blade.php
+++ resources/views/sections/left_sidebar.blade.php
@@ -133,7 +133,7 @@
                 <a href="#projects" data-toggle="collapse" aria-expanded="{{$is_project_active ? 'true' : 'false'}}"> <ion-icon name="rocket-outline"></ion-icon>Projets </a>
                 <ul class="collapse list-unstyled {{$is_project_active ? 'in' : ''}}" id="projects">
                     @if (in_array('projects', $modules))
-                        <li><a href="{{ route('admin.projects.index') }}" class="{{ request()->routeIs('admin.projects.index') ? 'active' : '' }}">Gestion de projets </a></li>
+                        <li><a href="{{ route('admin.projects.index') }}" class="{{ request()->routeIs('admin.projects.index') ? 'active' : '' }}">Liste des projets </a></li>
                     @endif
 
                     @if (in_array('tasks', $modules))
